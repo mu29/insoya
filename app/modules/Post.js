092291913757d483ef81dd1ce9b702c187325dff
InJung Chung <mu29@yeoubi.net>
2016-12-13 01:51:18 +0900

    `fetchPostList` 결과를 중복 검사하여 목록에 추가한다

--- app/modules/Post.js
+++ app/modules/Post.js
@@ -6,24 +6,30 @@ export const POST_SUCCEEDED = 'POST_SUCCEEDED';
 
 const defaultState = {
   posts: [],
+  post: {},
 };
 
 export default function (state = defaultState, action) {
   switch (action.type) {
     case POST_LIST_SUCCEEDED:
-      return { ...state, posts: action.posts };
+      let posts = [ ...state.posts, ...action.posts.map(p => ({ ...p, menu: action.menu })) ];
+      return {
+        ...state,
+        posts: posts.filter((obj, pos, arr) => (arr.map(mapObj => mapObj['title']).indexOf(obj['title']) === pos)),
+      };
     case POST_SUCCEEDED:
-      return { ...state, posts: [ ...state.posts, action.post ] };
+      return { ...state, post: action.post };
     default:
       return state;
   }
 }
 
-function fetchPostListSuccess(posts) {
+function fetchPostListSuccess(posts, menu) {
   return (dispatch) => {
     dispatch(hideProgress());
     dispatch({
       type: POST_LIST_SUCCEEDED,
+      menu,
       posts,
     });
   }
@@ -35,11 +41,11 @@ function fetchPostListFail() {
   }
 }
 
-export function fetchPostList(url) {
+export function fetchPostList(url, menu) {
   return (dispatch) => {
     PostApi.fetchPostList(url)
       .then((res) => {
-        dispatch(fetchPostListSuccess(res));
+        dispatch(fetchPostListSuccess(res, menu));
       })
       .catch(() => dispatch(fetchPostListFail()));
   };
