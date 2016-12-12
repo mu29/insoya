dd666aa0754be7c31edc16ab8056e09cc80e5584
InJung Chung <mu29@yeoubi.net>
2016-12-13 01:51:34 +0900

    백그라운드 프로그레스 바를 띄우는 `showBackgroundProgress` 액션을 추가한다

--- app/modules/Progress.js
+++ app/modules/Progress.js
@@ -1,16 +1,20 @@
 export const SHOW_PROGRESS = 'SHOW_PROGRESS';
+export const SHOW_BACKGROUND_PROGRESS = 'SHOW_BACKGROUND_PROGRESS';
 export const HIDE_PROGRESS = 'HIDE_PROGRESS';
 
 const defaultState = {
   showing: false,
+  backgroundShowing: false,
 };
 
 export default function (state = defaultState, action) {
   switch (action.type) {
     case SHOW_PROGRESS:
       return { ...state, showing: true };
+    case SHOW_BACKGROUND_PROGRESS:
+      return { ...state, backgroundShowing: true };
     case HIDE_PROGRESS:
-      return { ...state, showing: false };
+      return { ...state, showing: false, backgroundShowing: false };
     default:
       return state;
   }
@@ -19,6 +23,12 @@ export default function (state = defaultState, action) {
 export function showProgress() {
   return {
     type: SHOW_PROGRESS,
+  };
+}
+
+export function showBackgroundProgress() {
+  return {
+    type: SHOW_BACKGROUND_PROGRESS,
   };
 }
 
