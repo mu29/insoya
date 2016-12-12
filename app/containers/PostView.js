4c460f3a619c510041caa6b65956e39ba56aa739
InJung Chung <mu29@yeoubi.net>
2016-12-13 04:21:40 +0900

    `PostView` 에 로딩 애니메이션을 추가한다

--- app/containers/PostView.js
+++ app/containers/PostView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Platform, View, Text, ScrollView, StyleSheet } from 'react-native';
+import { Platform, Modal, ActivityIndicator, View, Text, ScrollView, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { readPost } from '../modules/Post';
 import { showProgress } from '../modules/Progress';
@@ -34,8 +34,11 @@ const styles = StyleSheet.create({
     borderTopWidth: 0.5,
     borderColor: '#E0E0E0',
   },
-  content: {
-
+  modal: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
 });
 
@@ -43,27 +46,40 @@ const BASE_URL = 'http://www.insoya.com/bbs';
 
 class PostView extends Component {
   componentDidMount() {
-    const { url, readPost } = this.props;
+    const { url, readPost, showProgress } = this.props;
+    showProgress();
     readPost(`${BASE_URL}/${url}`);
   }
 
   render() {
-    const { post, route, navigator } = this.props;
+    const { showing, post, route, navigator } = this.props;
     return (
       <View style={ styles.container }>
         <Navigator route={ route } navigator={ navigator } />
+        {
+          showing &&
+          <Modal
+            animationType={ "none" }
+            transparent={ false }
+            visible={ showing }
+          >
+            <View style={ styles.modal }>
+              <ActivityIndicator animating={ true } size="large" color="#fa5d63" />
+            </View>
+          </Modal>
+        }
         <ScrollView>
           <View>
             <View style={ styles.contentWrapper }>
               <Text style={ styles.title }>{ post.title }</Text>
-              <Text style={ styles.author }>{ post.author } | 2016.12.13 00:41 | 조회 630</Text>
+              <Text style={ styles.author }>{ `${post.author} | ${post.date} | 조회 ${post.count}` }</Text>
               <View style={ styles.line } />
-              <Text>asdfsadf{ post.content }</Text>
+              <Text>{ post.content }</Text>
               <View style={ styles.line } />
             </View>
             {
               post.commentList && post.commentList.map((comment, i) => (
-                <CommentItem key={ i } author={ comment.author } content={ comment.content } />
+                <CommentItem key={ i } comment={ comment } />
               ))
             }
           </View>
@@ -76,7 +92,7 @@ class PostView extends Component {
 export default connect(
   ({ Post, Progress }) => ({
     post: Post.post,
-    progress: Progress.showing,
+    showing: Progress.showing,
   }),
   { readPost, showProgress },
 )(PostView);
