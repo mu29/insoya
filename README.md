440e33a914658534a5172fe42f9967695739e9ca
InJung Chung <mu29@yeoubi.net>
2016-12-18 18:58:18 +0900

    설정에 INSOYA_HOST 상수를 추가한다

--- README.md
+++ README.md
@@ -9,4 +9,6 @@
 - [x] iOS 스플래시 추가
 - [x] 글에 띄어쓰기 제대로 적용하기 [참고](https://github.com/cheeriojs/cheerio/issues/839)
 - [x] Html entities 처리하기
+- [x] 댓글 Html entities 처리하기
+- [x] 코드 정리
 - [ ] 동영상 메뉴
--- app/config.js
+++ app/config.js
@@ -5,7 +5,9 @@ const env = process.env.NODE_ENV || 'development';
 config[env].mode = env;
 config[env].__MODE__ = env;
 config[env].API_ENTRY = config[env].api_entry;
+config[env].INSOYA_HOST = config[env].insoya_host;
 
 export default config[env];
 export const __MODE__ = env;
 export const API_ENTRY = config[env].api_entry;
+export const INSOYA_HOST = config[env].insoya_host;
