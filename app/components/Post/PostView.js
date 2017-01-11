a1f4ebb21ee0c2de0fa4b018c6108a91f31917e7
InJung Chung <mu29@yeoubi.net>
2017-01-11 22:36:21 +0900

    `PostView` 상단에 `StatusBar` 를 적용한다

--- app/components/Post/PostView.js
+++ app/components/Post/PostView.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Platform, Modal, ActivityIndicator, View, Text, Image, TouchableHighlight, ScrollView, StyleSheet } from 'react-native';
+import { Platform, StatusBar, Modal, ActivityIndicator, View, Text, Image, TouchableHighlight, ScrollView, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { AdMobBanner } from 'react-native-admob';
 import { readPost } from '../../modules/Post';
 import { showViewProgress } from '../../modules/Progress';
 import CommentItem from '../Item/CommentItem';
 import Navigation from '../Navigation';
+
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : 0;
 
 const styles = StyleSheet.create({
   container: {
@@ -90,6 +92,10 @@ const styles = StyleSheet.create({
     marginRight: 12,
     backgroundColor: '#FFF',
   },
+  statusBar: {
+    height: STATUSBAR_HEIGHT,
+    backgroundColor: '#FAFAFA',
+  },
 });
 
 const BASE_URL = 'http://www.insoya.com/bbs';
@@ -122,6 +128,9 @@ class PostView extends Component {
     const visible = { opacity: showing ? 0 : 255 };
     return (
       <View style={ styles.container }>
+        <View style={ styles.statusBar }>
+          <StatusBar backgroundColor="#FAFAFA" barStyle="dark-content"/>
+        </View>
         <Navigation route={ route } navigator={ navigator } />
         {
           showing &&
