1e2889b7d0d56b0eb4f3f47ca095e58ed526fb43
InJung Chung <mu29@yeoubi.net>
2017-01-12 20:53:58 +0900

    댓글 작성 액션을 추가한다

---
+++ app/modules/Comment.js
@@ -0,0 +1,80 @@
+import CommentApi from '../network/CommentApi';
+import { addComment } from './Post';
+
+export const COMMENT_SUCCESS = 'COMMENT_SUCCESS';
+export const COMMENT_FAIL = 'COMMENT_FAIL';
+export const SHOW_COMMENT_PROGRESS = 'SHOW_COMMENT_PROGRESS';
+export const HIDE_COMMENT_PROGRESS = 'HIDE_COMMENT_PROGRESS';
+export const CLEAR_MESSAGE = 'CLEAR_MESSAGE';
+
+const defaultState = {
+  message: '',
+  progress: false,
+};
+
+export default function (state = defaultState, action) {
+  switch (action.type) {
+    case COMMENT_SUCCESS:
+      return { ...state, message: '정상적으로 작성되었습니다.' };
+    case COMMENT_FAIL:
+      return { ...state, message: '댓글 작성에 실패했습니다.' };
+    case SHOW_COMMENT_PROGRESS:
+      return { ...state, progress: true };
+    case HIDE_COMMENT_PROGRESS:
+      return { ...state, progress: false };
+    case CLEAR_MESSAGE:
+      return { ...state, message: '' };
+    default:
+      return state;
+  }
+}
+
+function commentSuccess(content) {
+  return (dispatch) => {
+    dispatch(addComment(content))
+    dispatch(hideCommentProgress());
+    dispatch({
+      type: COMMENT_SUCCESS,
+    });
+  }
+}
+
+function commentFail() {
+  return (dispatch) => {
+    dispatch(hideCommentProgress());
+    dispatch({
+      type: COMMENT_FAIL,
+    });
+  }
+}
+
+export function createComment(id, password, referer, content) {
+  return (dispatch) => {
+    dispatch(showCommentProgress());
+    CommentApi.createComment(id, password, referer, content)
+      .then((res) => {
+        dispatch(commentSuccess(content));
+      })
+      .catch(() => dispatch(commentFail()));
+  };
+}
+
+export function showCommentProgress() {
+  return {
+    type: SHOW_COMMENT_PROGRESS,
+  };
+}
+
+export function hideCommentProgress() {
+  return {
+    type: HIDE_COMMENT_PROGRESS,
+  };
+}
+
+export function clearMessage() {
+  return (dispatch) => {
+    dispatch({
+      type: CLEAR_MESSAGE,
+    });
+  };
+}
---
+++ app/network/CommentApi.js
@@ -0,0 +1,14 @@
+import RestClient from '../helpers/RestClient';
+
+export default class CommentApi {
+  static createComment(id, password, referer, content) {
+    console.log(id, password, referer,  content);
+    return RestClient.instance.send({
+      url: '/comment',
+      data: {
+        method: 'POST',
+        body: JSON.stringify({ id, password, referer, content }),
+      },
+    });
+  }
+}
