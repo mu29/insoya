61b85864b807c407f79f27ce8cae37600724608c
InJung Chung <mu29@yeoubi.net>
2017-01-12 11:20:31 +0900

    덧글 스타일을 수정한다

--- app/components/Item/PostItem.js
+++ app/components/Item/PostItem.js
@@ -12,11 +12,16 @@ const styles = StyleSheet.create({
     marginBottom: -4,
     backgroundColor: '#FFF',
   },
+  topContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
   bottomContainer: {
     flex: 1,
     flexDirection: 'row',
   },
   title: {
+    flex: 1,
     color: '#000',
     fontSize: 16,
     marginBottom: 4,
@@ -24,6 +29,21 @@ const styles = StyleSheet.create({
   subtitle: {
     flex: 1,
     fontSize: 12,
+    color: '#999999',
+  },
+  commentWrapper: {
+    marginLeft: 8,
+    borderColor: '#999999',
+    borderRadius: 4,
+    borderWidth: 0.5,
+    paddingLeft: 4,
+    paddingRight: 4,
+    height: 14,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  comment: {
+    fontSize: 8,
     color: '#999999',
   },
   category: {
@@ -44,6 +64,8 @@ export default class PostItem extends Component {
 
   render() {
     const { title, url, comment, author, date, count, category } = this.props;
+    const hotComment = Number(comment) > 9 ? { color: '#FA5D63' } : {};
+    const hotCommentWrapper = Number(comment) > 9 ? { borderColor: '#FA5D63' } : {};
     return (
       <View style={ styles.container }>
         <TouchableHighlight
@@ -51,16 +73,26 @@ export default class PostItem extends Component {
           onPress={ () => this.onClick(url) }
         >
           <View>
-            <Text style={ styles.title }>
-              { `${title}${comment !== '' && ` [${comment}]` || ''}` }
-            </Text>
-            <View style={ styles.bottomContainer }>
-              <Text style={ styles.subtitle }>
-                { `${author} | ${date} ${count !== '' && `| 조회 ${count}` || ''}` }
+            <View style={ styles.topContainer }>
+              <Text style={ styles.title }>
+                { title }
               </Text>
               <Text style={ styles.category }>
                 { category }
               </Text>
+            </View>
+            <View style={ styles.bottomContainer }>
+              <Text style={ styles.subtitle }>
+                { `${author} | ${date} ${count !== '' && `| 조회 ${count}` || ''}` }
+              </Text>
+              {
+                comment !== '' &&
+                <View style={ [styles.commentWrapper, hotCommentWrapper] }>
+                  <Text style={ [styles.comment, hotComment] }>
+                    { comment }
+                  </Text>
+                </View>
+              }
             </View>
           </View>
         </TouchableHighlight>
